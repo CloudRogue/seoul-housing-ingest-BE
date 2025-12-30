4b8d14e435e828621fe020585620c8dc0dcd65f6
1ShinSuHo1 <sshsh1004@naver.com>
2025-12-30 14:50:47 +0900

    chore: initial commit

---
+++ src/main/java/com/seoulhousing/ingest_core/IngestCoreApplication.java
@@ -0,0 +1,13 @@
+package com.seoulhousing.ingest_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seoulhousing/ingest_core/IngestCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoulhousing.ingest_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
